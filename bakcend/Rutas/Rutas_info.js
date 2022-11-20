04344e2b98a541a48994741683355056c02b7345
jpmendez29 <juan.pablomendez@hotmail.com>
2022-11-20 18:34:03 -0500

    Cambios (Rutas y querys)

--- bakcend/Rutas/Rutas_info.js
+++ bakcend/Rutas/Rutas_info.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 var connection = require('../Db/database.js');
 
-router.post('/Info', async(req,res) => {
+router.post('/InfoP', async(req,res) => {
     const sql = 'SELECT usuarios.Usuario, usuarios.Contraseña, usuarios.E_mail, usuarios.Num_cuenta, informacion_personal.Nombre, informacion_personal.Edad, informacion_personal.Fecha_nacimiento, informacion_personal.Cedula FROM usuarios INNER JOIN informacion_personal ON usuarios.Id_info = informacion_personal.Id_info where usuarios.Usuario = ?;'
-    const usr = req.body.user;
+    const usr = req.query.user;
     try { 
         connection.query(sql,[usr], function(err,results){
         res.send(JSON.stringify(results, null, 4));
@@ -15,5 +15,39 @@ router.post('/Info', async(req,res) => {
     }
 })
 
+
+router.get('/Historial', async (req, res) => {
+    const usr = req.session.all
+    const sql = "SELECT * FROM db_pagos.transacciones where Id_cuenta_entrega = ? or Id_cuenta_recibido = ?"
+    try {
+        connection.query(sql, [usr.Num_cuenta, usr.Num_cuenta], function(err, results, fields) {
+            if(results.length == 0 || !(usr.Num_cuenta === results[0].Id_cuenta_entrega || usr.Num_cuenta === results[0].Id_cuenta_recibido)) {
+                res.status(203)
+                throw err
+            }else{
+                connection.query("SELECT Usuario as Usuario_envia, transacciones.Cantidad as transaccion FROM usuarios inner join transacciones ON usuarios.Id_cuenta = transacciones.Id_cuenta_entrega",function(err,results){
+                    //res.send(JSON.stringify(results, null, 4))
+                })
+                connection.query("SELECT Usuario as Usuario_recibe FROM usuarios inner join transacciones ON usuarios.Id_cuenta = transacciones.Id_cuenta_recibido",function(err,results){
+                    //res.send(JSON.stringify(results, null, 4))
+                })
+            res.status(302)
+            res.send(JSON.stringify(results, null, 4))
+            }
+        })
+    } catch (error) {
+        res.status(404)
+        return error
+    }
+});
+
+router.get("/Saldo", async (req, res) => {
+    const usr = req.session.all
+    connection.query("SELECT tarjeta.Nombre_tarjeta, tarjeta.Num_tarjeta, tipo_tarjeta.Tipo, if(tipo_tarjeta.Debito_credito=0, 'Debito', 'Credito') as Debito_credito, cuenta_bancaria.Cantidad FROM db_pagos.cuenta_bancaria LEFT JOIN db_pagos.tarjeta ON db_pagos.cuenta_bancaria.Id_tarjeta = db_pagos.tarjeta.Id_tarjeta LEFT JOIN db_pagos.tipo_tarjeta ON db_pagos.tipo_tarjeta.Id_tipo_tarjeta = db_pagos.tarjeta.Id_tipo_tarjeta where Num_cuenta = ?", [Number(usr.Num_cuenta)], function(err, results){
+        res.status(202)
+        console.log(results[0])
+        res.send(JSON.stringify(results, null, 4))
+    })
+})
 
 module.exports = router;
--- bakcend/Rutas/Rutas_trans.js
+++ bakcend/Rutas/Rutas_trans.js
@@ -5,31 +5,6 @@ const router = express.Router()
 var connection = require("../Db/database.js")
 
 
-/*router.get('/History', async (req, res) => {
-    const usr = req.session.all
-    const sql = "SELECT * FROM db_pagos.transacciones where Id_cuenta_entrega = ? or Id_cuenta_recibido = ?"
-    try {
-        connection.query(sql, [usr.Id_cuenta, usr.Id_cuenta], function(err, results, fields) {
-            if(results.length == 0 || !(usr.Id_cuenta === results[0].Id_cuenta_entrega || usr.Id_cuenta === results[0].Id_cuenta_recibido)) {
-                res.status(203)
-                throw err
-            }else{
-                connection.query("SELECT Usuario as Usuario_envia, transacciones.Cantidad as transaccion FROM usuarios inner join transacciones ON usuarios.Id_cuenta = transacciones.Id_cuenta_entrega",function(err,results){
-                    //res.send(JSON.stringify(results, null, 4))
-                })
-                connection.query("SELECT Usuario as Usuario_recibe FROM usuarios inner join transacciones ON usuarios.Id_cuenta = transacciones.Id_cuenta_recibido",function(err,results){
-                    //res.send(JSON.stringify(results, null, 4))
-                })
-            res.status(302)
-            res.send(JSON.stringify(results, null, 4))
-            }
-        })
-    } catch (error) {
-        res.status(404)
-        return error
-    }
-});*/
-
 router.post('/Transaccion', async(req,res) => {
     const usr = req.session.all
     
@@ -44,9 +19,26 @@ router.post('/Transaccion', async(req,res) => {
     const fv = req.body.fv 
     const cuot =  req.body.cuot
 
+    /*  const usr = req.session.all
+    
+    const ac = req.query.ac
+    const cant = req.query.cant
+    
+    const conp = req.query.conp
+
+    const tarj = req.query.tarj
+    const csv = req.query.csv
+
+    const fv = req.query.fv 
+    const cuot =  req.query.cuot */
+
     connection.query("WITH ab as (SELECT tarjeta.Nombre_tarjeta, tarjeta.Num_tarjeta, tarjeta.Codigo_seg, tarjeta.Fecha_vencimiento, tipo_tarjeta.Tipo, if(tipo_tarjeta.Debito_credito=0, 'Debito', 'Credito') as Debito_credito, cuenta_bancaria.Cantidad FROM db_pagos.cuenta_bancaria LEFT JOIN tarjeta ON db_pagos.cuenta_bancaria.Id_tarjeta = db_pagos.tarjeta.Id_tarjeta LEFT JOIN db_pagos.tipo_tarjeta ON db_pagos.tipo_tarjeta.Id_tipo_tarjeta = db_pagos.tarjeta.Id_tipo_tarjeta where Num_cuenta = ?) select * from ab where Num_tarjeta = ?", [Number(usr.Num_cuenta),tarj], function(err, results){
         const a = results
-        if(a.Codigo_seg == csv){
+        console.log(a)
+
+        console.log(csv)
+
+        if(a[0].Codigo_seg == csv){
             if(results.length == 0) {
                 console.log("la tarjeta no se encuentra")
                 res.send(JSON.stringify("la tarjeta no se encuentra", null, 4))
@@ -102,8 +94,8 @@ router.post('/Transaccion', async(req,res) => {
 
                                 })
                             }else{
-                                console.log("salgo insuficiente")
-                                res.send(JSON.stringify("salgo insuficiente", null, 4))
+                                console.log("saldo insuficiente")
+                                res.send(JSON.stringify("saldo insuficiente", null, 4))
                             }
                         }
 
@@ -116,15 +108,6 @@ router.post('/Transaccion', async(req,res) => {
         }
     })
     
-})
-
-
-router.get("/Saldo", async (req, res) => {
-    const usr = req.session.all
-    connection.query("SELECT tarjeta.Nombre_tarjeta, tarjeta.Num_tarjeta, tipo_tarjeta.Tipo, if(tipo_tarjeta.Debito_credito=0, 'Debito', 'Credito') as Debito_credito, cuenta_bancaria.Cantidad FROM db_pagos.cuenta_bancaria LEFT JOIN db_pagos.tarjeta ON db_pagos.cuenta_bancaria.Id_tarjeta = db_pagos.tarjeta.Id_tarjeta LEFT JOIN db_pagos.tipo_tarjeta ON db_pagos.tipo_tarjeta.Id_tipo_tarjeta = db_pagos.tarjeta.Id_tipo_tarjeta where Num_cuenta = ?", [Number(usr.Num_cuenta)], function(err, results){
-        res.status(202)
-        res.send(JSON.stringify(results, null, 4))
-    })
 })
 
 module.exports = router;
