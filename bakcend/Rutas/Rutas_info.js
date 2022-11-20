fb52f8a96f72859d7cdb1e4b26c5bcd4d15e8ce2
jpmendez29 <juan.pablomendez@hotmail.com>
2022-11-20 18:51:39 -0500

    request -- body

--- bakcend/Rutas/Rutas_info.js
+++ bakcend/Rutas/Rutas_info.js
@@ -4,7 +4,7 @@ var connection = require('../Db/database.js');
 
 router.post('/InfoP', async(req,res) => {
     const sql = 'SELECT usuarios.Usuario, usuarios.Contraseña, usuarios.E_mail, usuarios.Num_cuenta, informacion_personal.Nombre, informacion_personal.Edad, informacion_personal.Fecha_nacimiento, informacion_personal.Cedula FROM usuarios INNER JOIN informacion_personal ON usuarios.Id_info = informacion_personal.Id_info where usuarios.Usuario = ?;'
-    const usr = req.query.user;
+    const usr = req.body.user;
     try { 
         connection.query(sql,[usr], function(err,results){
         res.send(JSON.stringify(results, null, 4));
