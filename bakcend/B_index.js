9dbe94a4b6919cf3fad60d9120c78b87ce1fd22d
Jonathan Enrique Arias Rua <jony1754@hotmail.com>
2022-11-20 16:48:27 -0500

    shit

--- bakcend/B_index.js
+++ bakcend/B_index.js
@@ -1,32 +1,32 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const session = require('express-session')
-
-
+const session = require('express-session');
+const cors = require('cors');
 // MIDDLEWARE JSON
 app.use(express.json());
-
+app.use(cors());
 // var sesion
-app.use(session({
+
+app.use(
+  session({
     secret: 'secret',
     resave: true,
-    saveUninitialized: true
-}))
-
+    saveUninitialized: true,
+  })
+);
 
 // ROUTES
-const routeLog = require('./Rutas/Rutas_logs')
-const routetrans = require('./Rutas/Rutas_trans')
-app.use("/Login", routeLog)
-app.use("/Transacciones", routetrans)
-
+const routeLog = require('./Rutas/Rutas_logs');
+const routetrans = require('./Rutas/Rutas_trans');
+app.use('/Login', routeLog);
+app.use('/Transacciones', routetrans);
 
 // open app port 3000
-app.listen(3000, (err) => {
-    if(err){
-        console.log("Error", err)
-        return
-    }
-    console.log("listening on port 3000")
+app.listen(3005, (err) => {
+  if (err) {
+    console.log('Error', err);
+    return;
+  }
+  console.log('listening on port 3000');
 });
--- bakcend/Rutas/Rutas_logs.js
+++ bakcend/Rutas/Rutas_logs.js
@@ -1,38 +1,39 @@
-const express = require('express')
-const router = express.Router()
-var connection = require("../Db/database.js")
-
+const express = require('express');
+const router = express.Router();
+var connection = require('../Db/database.js');
 
 router.post('/Login', async (req, res) => {
-    const usr = req.query.user
-    const pasw = req.query.pasw
-    const sql = "SELECT * FROM db_pagos.usuarios WHERE Usuario = ?"
-    try {
-        connection.query(sql, [usr], function(err, results, fields) {
-            console.log(results)
-            if(results.length == 0 || !(pasw === results[0].Contraseña)) {
-                res.status(203)
-                throw err
-            }else{
-            req.session.loggedin = true;
-            req.session.usr = results[0].Usuario
-            req.session.all = results[0]
-            res.status(302)
-            res.send(JSON.stringify(results, null, 4))
-            }
-        })
-    } catch (error) {
-        res.status(404)
-        return error
-    }
+  console.log(req.body);
+  const usr = req.body.user;
+  const pasw = req.body.pasw;
+  console.log(usr, pasw, 'login');
+  const sql = 'SELECT * FROM db_pagos.usuarios WHERE Usuario = ?';
+  try {
+    connection.query(sql, [usr], function (err, results, fields) {
+      console.log(results);
+      if (results.length == 0 || !(pasw === results[0].Contraseña)) {
+        res.status(203);
+        throw err;
+      } else {
+        req.session.loggedin = true;
+        req.session.usr = results[0].Usuario;
+        req.session.all = results[0];
+        res.status(200);
+        res.send(JSON.stringify(results, null, 4));
+      }
+    });
+  } catch (error) {
+    res.status(404);
+    return error;
+  }
 });
 
 router.get('/Logout', (req, res) => {
-    console.log(req.session.all.Id_cuenta)
-    req.session.destroy(()=>{
-        console.log("salio")
-        res.status(202)
-    })
+  console.log(req.session.all.Id_cuenta);
+  req.session.destroy(() => {
+    console.log('salio');
+    res.status(202);
+  });
 });
 
 module.exports = router;
