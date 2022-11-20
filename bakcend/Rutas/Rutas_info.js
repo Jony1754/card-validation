3bef381c62d991e203a46d0ed170b3a351493bf6
jpmendez29 <juan.pablomendez@hotmail.com>
2022-11-20 17:45:31 -0500

    cambios query info

--- bakcend/Rutas/Rutas_info.js
+++ bakcend/Rutas/Rutas_info.js
@@ -3,14 +3,12 @@ const router = express.Router();
 var connection = require('../Db/database.js');
 
 router.post('/Info', async(req,res) => {
-    const sql = 'SELECT * FROM db_pagos.informacion_personal where Id_info = ?;'
+    const sql = 'SELECT usuarios.Usuario, usuarios.Contraseña, usuarios.E_mail, usuarios.Num_cuenta, informacion_personal.Nombre, informacion_personal.Edad, informacion_personal.Fecha_nacimiento, informacion_personal.Cedula FROM usuarios INNER JOIN informacion_personal ON usuarios.Id_info = informacion_personal.Id_info where usuarios.Usuario = ?;'
     const usr = req.body.user;
     try { 
-    connection.query('SELECT Id_info FROM db_pagos.usuarios WHERE Usuario = ?', [usr], function(err,results){
-        connection.query(sql,[results[0].Id_info], function(err,results){
-            res.send(JSON.stringify(results, null, 4));
+        connection.query(sql,[usr], function(err,results){
+        res.send(JSON.stringify(results, null, 4));
         })  
-    })
     }catch (error){
         res.status(404);
         return error;
